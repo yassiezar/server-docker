01020f0e6ff0d88e737166d38f98ba932458b065
Jaycee <jaycee.lock@gmail.com>
2022-08-11 21:39:33 +0100

    Cron job now sets correct file permissions

--- web/update_cv_cron.sh
+++ web/update_cv_cron.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 
 wget -O ScalewayWebsite/static/ScalewayWebsite/cv/jc_lock_cv.pdf https://github.com/yassiezar/cv/releases/download/latest/jc_lock_cv.pdf
+chmod 644 ScalewayWebsite/static/ScalewayWebsite/cv/jc_lock_cv.pdf 
